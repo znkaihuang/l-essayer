7fb009e217cff3cfe99e374996030ba1ab5fcd25
znkaihuang <zkhuang@protonmail.com>
2021-11-09 21:53:03 +0800

    Add "Verb" class inheriting "Word" class
    Change-Id: Ieac68adbf099120453842c87801fdfa50dfe675e
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

---
+++ Lessayer/lib/src/main/java/lessayer/word/Verb.java
@@ -0,0 +1,11 @@
+package lessayer.word;
+
+public class Verb extends Word {
+
+	public Verb(String word) {
+		// TODO Auto-generated constructor stub
+		super(word);
+		this.setType("verb");
+	}
+
+}
