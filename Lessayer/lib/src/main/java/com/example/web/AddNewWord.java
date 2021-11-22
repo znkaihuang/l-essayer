9f725f108e19daf3152b64ece8f712d8ed2f6d83
znkaihuang <zkhuang@protonmail.com>
2021-11-22 16:06:49 +0800

    AddNewWord servlet implements the function that store word to database.
    Change-Id: Ifc9ea5d3d3638e38b67c6303fdb382686dfce341
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- Lessayer/lib/src/main/java/com/example/web/AddNewWord.java
+++ Lessayer/lib/src/main/java/com/example/web/AddNewWord.java
@@ -3,6 +3,9 @@ package com.example.web;
 import com.example.model.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import lessayer.word.*;
+import lessayer.word.ReadWriteWord;
+
 import java.io.*;
 import java.util.*;
 
@@ -12,12 +15,17 @@ public class AddNewWord extends HttpServlet {
 			HttpServletResponse response)
 			throws IOException, ServletException {
 		
-		// BeerExpert be = new BeerExpert();
+		request.setCharacterEncoding("UTF-8");
 		String word = request.getParameter("word");
 		String type = request.getParameter("type").toUpperCase();
 		String conjug_declin = request.getParameter("conjug_declin");
 		String definition = request.getParameter("definition");
 		String example = request.getParameter("example");
+		
+		Word newWord = setWordTypeAndConjugDeclin(word, type, conjug_declin);
+		newWord.setDefinition(definition);
+		newWord.setExampleSentence(example);
+		ReadWriteWord.saveWordToDatabase(newWord);
 		
 		request.setAttribute("word", word);
 		request.setAttribute("type", type);
@@ -27,5 +35,37 @@ public class AddNewWord extends HttpServlet {
 		
 		RequestDispatcher view = request.getRequestDispatcher("addWord.jsp");
 		view.forward(request, response);
+	}
+
+	private Word setWordTypeAndConjugDeclin(String word, String type, String conjug_declin) {
+		// TODO Auto-generated method stub
+		Word returnWord = null;
+		if(type.compareTo("MASCULIN-NOUN") == 0) {
+			Noun noun = new Noun(word);
+			HashMap<String, String> property = new HashMap<>();
+			property.put("gender", "masculin");
+			noun.setProperty(property);
+			returnWord = noun;
+		}
+		else if (type.compareTo("FEMININ-NOUN") == 0) {
+			Noun noun = new Noun(word);
+			HashMap<String, String> property = new HashMap<>();
+			property.put("gender", "féminin");
+			noun.setProperty(property);
+			returnWord = noun;
+		}
+//		else if (type.compareTo("verb") == 0) {
+//			Verb verb = new Verb(word);
+//			returnWord = verb;
+//		}
+//		else if (type.compareTo("adjective") == 0) {
+//			Adjective adjective = new Adjective(word);
+//			returnWord = adjective;
+//		}
+//		else if (type.compareTo("adverb") == 0) {
+//			Adverb adverb = new Adverb(word);
+//			returnWord = adverb;
+//		}
+		return returnWord;
 	}
 }
---
+++ Lessayer/lib/src/test/java/com/example/web/AddNewWordTest.java
@@ -0,0 +1,14 @@
+package com.example.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddNewWordTest {
+
+	@Test
+	void testDoPostHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}
