c3c6b3afa9a98233f5a0e6aed6c7103d3b39cd03
znkaihuang <zkhuang@protonmail.com>
2021-11-25 15:56:21 +0800

    Servlet Test Judge
    Change-Id: Ibc98f0e3e42cfb158bbd93b1ceb334efc7669a36
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- Lessayer/lib/src/main/java/com/example/web/TestJudge.java
+++ Lessayer/lib/src/main/java/com/example/web/TestJudge.java
@@ -1,6 +1,9 @@
 package com.example.web;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import com.example.model.PrepareQuestion;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -17,13 +20,36 @@ public class TestJudge extends HttpServlet {
 		
 		request.setCharacterEncoding("UTF-8");
         Cookie[] cookies = request.getCookies();
+        
+        ArrayList <String> correctAnswers = new ArrayList<>();
+        ArrayList <String> userAnswers = new ArrayList<>();
+        ArrayList <String> results = new ArrayList<>();
         if(cookies != null) {
         	for(int i = 0; i < cookies.length; i++) {
         		Cookie cookie = cookies[i];
-   				System.out.println(cookie.getValue());
+        		if(cookie.getName().matches("correct-answer-[1-9][0-9]*")) {
+        			correctAnswers.add(cookie.getValue());
+        		}
         	}
         }
-		
+        
+        for(int questionId = 1; questionId <= PrepareQuestion.getQuestionNum(); questionId++ ) {
+        	userAnswers.add(request.getParameter("user-answer-" + String.valueOf(questionId)));
+        }
+        
+        int score = 0;
+        for(int i = 0; i < userAnswers.size(); i++) {
+        	String judgement = "Votre réponse est \"" + userAnswers.get(i) + "\".<br>" +
+        			"La bonne réponse est \"" + correctAnswers.get(i) + "\".<br>";
+        	results.add(judgement);
+        	if(correctAnswers.get(i).equals(userAnswers.get(i))) {
+        		score++;
+        	}
+        }
+        
+        request.setAttribute("results", results);
+        request.setAttribute("score", score);
+        
         RequestDispatcher view = request.getRequestDispatcher("quiz-score.jsp");
 		view.forward(request, response);
 	}
